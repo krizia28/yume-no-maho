687c7ae48d7c723bb7fabe1a152e5fd24662ba4e
YumeNoMaho <56683795+krizia28@users.noreply.github.com>
2026-03-14 07:07:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-let citations = [
+document.addEventListener("DOMContentLoaded", function() {
+
+const citations = [
 "Plus la nuit est noire, plus les étoiles brillent.",
 "Le cœur d’un homme peut être plus dangereux qu’un démon.",
 "Les rêves sont la première forme de magie."
 ];
 
-let random = Math.floor(Math.random() * citations.length);
+const random = Math.floor(Math.random() * citations.length);
 
 document.getElementById("citation").innerText = citations[random];
 
+});
 
-function toggleContact() {
+
+function toggleContact(){
 
 const contact = document.getElementById("contact-info");
 
-if(contact.classList.contains("contact-visible")){
-
-contact.classList.remove("contact-visible");
-
-}else{
-
-contact.classList.add("contact-visible");
-
-}
+contact.classList.toggle("contact-visible");
 
 }
